07e24fe90d572bc50fa0ad6764c865d52835ae95
qandries <andries.quentin@gmail.com>
2018-11-13 10:46:56 +0100

    add script npm

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
 app.use(
     '/client',
     express.static(__dirname + '/public')
